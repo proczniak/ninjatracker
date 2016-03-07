3b2c10d65f6238d13e4a98c8ab0b6aec0344870b
proczniak <proczniak@gmail.com>
2016-03-07 13:19:49 +0100

    added some login realted functionality

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -1,18 +1,36 @@
 App = React.createClass({
   mixins: [ReactMeteorData],
-  getMeteorData(){ //assigns variables, objects (stuff) to this.props.data.loggedIn
+  getMeteorData(){ //assigns variables, objects (stuff) to this.props.data.property - (loggedIn)
     return{
       loggedIn: !!Meteor.user()
     }
   },
+  showLayout(){
+    return(
+        <div className="row">
+            <div className="col-xs-3">
+                {this.props.nav}
+            </div>
+            <div className="col-xs-9">
+                {this.props.content}
+            </div>
+        </div>
+    )
+  },
+  showLogin() {
+      return (
+          <div className="row">
+              <div className="col-xs-12 text-center">
+                  <p>You must be logged in to do that.</p>
+              </div>
+          </div>
+      )
+  },
   render() {
     return(
       <div className="container-fluid main-container">
-        <div className="col-xs-3">
-          {this.props.nav}
-        </div>
-        <div className="col-xs-9">
-          {this.props.content}
+        <div className="row">
+            {this.data.loggedIn ? this.showLayout() : this.showLogin()}
         </div>
       </div>
     )
