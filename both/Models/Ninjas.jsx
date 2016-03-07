b552daa9a4589bb5be8e551bb34b86359d90c0e7
Piotr Roczniak <proczniak@gmail.com>
2016-03-07 17:11:25 +0100

    Data Structures have been set.

---
+++ both/Models/Ninjas.jsx
@@ -0,0 +1,41 @@
+Ninjas = new Mongo.Collection('ninjas');
+
+var Schemas = {};
+Schemas.Ninja = new SimpleSchema({
+  firstName: {
+    type: String,
+    label: "First Name",
+    max: 25
+  },
+  lastName: {
+    type: String,
+    label: "Last Name",
+    max: 25,
+    index: true,
+    unique: true
+  },
+  score: {
+    type: Number,
+    label: "Score",
+    min: 0
+  },
+  status: {
+    type: Boolean,
+    label: "Status"
+  },
+  jobsCompleted: {
+    type: Number,
+    label: "Jobs Completed",
+    min: 0
+  }
+});
+
+Ninjas.attachSchema(Schemas.Ninja);
+
+Ninjas.helpers({
+  fullName: function() {
+    return this.firstName + ' ' + this.lastName;
+  }
+});
+
+Ninjas.permit(['insert', 'update', 'remove']).ifLoggedIn().apply();
